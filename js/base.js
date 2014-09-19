69b9d594b0386ba2ba41b9d462a7920e9945a9eb
Axel Tanner <axeltanner38@gmail.com>
2014-09-19 12:22:54 -0700

    google map

--- js/base.js
+++ js/base.js
@@ -4,3 +4,14 @@ $('a').click(function(){
     }, 1000);
     return false;
 });
+
+function initialize() {
+        var mapCanvas = document.getElementById('mappic');
+        var mapOptions = {
+          center: new google.maps.LatLng(44.5403, -78.5463),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
